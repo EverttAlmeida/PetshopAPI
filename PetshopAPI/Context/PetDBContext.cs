6a48e2c2d566a9ef5989ffea402d87e420c0b4c8
tarde.cetafaju <tarde.cetafaju@PC03LAB2519>
2024-02-19 17:20:51 -0300

    Adicionar arquivos de projeto.

---
+++ PetshopAPI/Context/PetDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetshopAPI.Context
+{
+    public class PetDBContext:DbContext
+    {
+        public PetDBContext(DbContextOptions<PetDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Models.Pet> Pets { get; set; }
+
+        
+    }
+}
---
+++ PetshopAPI/Models/Pet.cs
@@ -0,0 +1,15 @@
+﻿namespace PetshopAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public string Genero { get; set; }
+        public int Idade { get; set; }
+
+
+
+
+    }
+}
